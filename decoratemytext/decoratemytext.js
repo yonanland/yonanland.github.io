4b82ed6cb6a88580f6cad37e931c36eaed11af5e
yonanland <yonininu@gmail.com>
2023-05-07 23:23:31 -0500

    updates in all the .tml and .css & add new files

---
+++ decoratemytext/decoratemytext.js
@@ -0,0 +1,34 @@
+// alert("Hello, World");
+
+function onclick(){
+    alert("Hello, world!");
+} 
+
+function biggerdecorations() {
+  const textArea = document.getElementById("text");
+
+  if (textArea.style.fontSize === "24pt") {
+    textArea.style.fontSize = "medium";
+  } else {
+    textArea.style.fontSize = "24pt";
+  }
+}
+
+
+function bling() {
+    const checkBox = document.getElementById("check");
+    const textArea = document.getElementById("text");
+  
+    if (checkBox.checked) {
+      textArea.style.fontWeight = "bold";
+      textArea.style.color = "green";
+      textArea.style.textDecoration = "underline";
+    //   document.body.style.backgroundImage = "url('https://media.giphy.com/media/3o7qE3vzVBKjhuHp0g/giphy.gif')";
+    } else {
+      textArea.style.fontWeight = "normal";
+      textArea.style.color = "inherit";
+      textArea.style.textDecoration = "none";
+    //   document.body.style.backgroundImage = "none";
+    }
+
+}
