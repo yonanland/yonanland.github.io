64fe6a65ae8d1ce760382724d0a894f4b3a8859e
yonanland <yonininu@gmail.com>
2023-05-17 10:25:20 -0500

    updates on Simple_calculator

--- simple_calculator/simple_calculatorMod.js
+++ simple_calculator/simple_calculatorMod.js
@@ -29,7 +29,10 @@ exports.displays = function (req, res, vals) {
   res.write(String(result));
   res.write("</p>");
   res.write(
-    <div class="responder">"<a href='https://yonanland.github.io/simple_calculator/simplecalculator.html'>To Perform another calculation <br/> Click me! </a>"</div>
+    <div class="responder"> 
+    <a href='https://yonanland.github.io/simple_calculator/simplecalculator.html'>
+      To Perform another calculation <br/> Click me! </a>"
+    </div>
   );
 
 
