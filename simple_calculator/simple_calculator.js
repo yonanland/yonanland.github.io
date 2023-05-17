ac69d7657946487c994459d6e6eb17dea6f21afe
yonanland <yonininu@gmail.com>
2023-05-17 09:39:07 -0500

    updates on Simple_calculator

--- simple_calculator/simple_calculator.js
+++ simple_calculator/simple_calculator.js
@@ -1,29 +1,29 @@
 var http = require("http");
 var url = require("url");
 var fs = require("fs");
-var simple_calculatormod = require("./simple_calculatorMod");
+var calcMod = require("./simple_calculatorMod");
 
 http
   .createServer(function (req, res) {
     var q = url.parse(req.url, true);
     var qdata = q.query;
     var filename = "." + q.pathname;
-    if (q.pathname == "sim") {
+    if (q.pathname == "/simple_calculator.js") {
       if (qdata.operation == "add") {
-        simple_calculatormod.add(req, res, q.query);
-        simple_calculatormod.displays(req, res, q.query);
+        calcMod.add(req, res, q.query);
+        calcMod.displays(req, res, q.query);
       } else if (qdata.operation == "subtract") {
-        simple_calculatormod.subtract(req, res, q.query);
-        simple_calculatormod.displays(req, res, q.query);
+        calcMod.subtract(req, res, q.query);
+        calcMod.displays(req, res, q.query);
       } else if (qdata.operation == "multiply") {
-        simple_calculatormod.multiply(req, res, q.query);
-        simple_calculatormod.displays(req, res, q.query);
+        calcMod.multiply(req, res, q.query);
+        calcMod.displays(req, res, q.query);
       } else if (qdata.operation == "division") {
-        simple_calculatormod.division(req, res, q.query);
-        simple_calculatormod.displays(req, res, q.query);
+        calcMod.division(req, res, q.query);
+        calcMod.displays(req, res, q.query);
       } else if (qdata.operation == "modules") {
-        simple_calculatormod.modules(req, res, q.query);
-        simple_calculatormod.displays(req, res, q.query);
+        calcMod.modules(req, res, q.query);
+        calcMod.displays(req, res, q.query);
       }
     } else
       fs.readFile(filename, function (err, data) {
@@ -37,24 +37,3 @@ http
       });
   })
   .listen(8090);
-
-exports.displays = function (req, res, vals) {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<!DOCTYPE html>");
-  res.write("<html>");
-  res.write('<head><meta charset="utf-8"/>');
-  res.write("<title>Calculator Web Site</title>");
-  res.write("</head>");
-  res.write("<body>");
-  res.write(
-    '<p style="background-color: lightgray" ; style="font:1.5rem bolder";> The output is: '
-  );
-  res.write(String(result));
-  res.write("</p>");
-  res.write(
-    "<a href='https://yonanland.github.io/simple_calculator/simplecalculator.html'>To Perform another calculation</a>"
-  );
-  res.write("</body>");
-  res.write("</html>");
-  return res.end();
-};
