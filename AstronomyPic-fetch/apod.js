a7d0251d837636a891f382d0f1b514d5dc82f861
yonanland <yonininu@gmail.com>
2023-05-22 10:08:28 -0500

    updates on astronomy pic fetch

--- AstronomyPic-fetch/apod.js
+++ AstronomyPic-fetch/apod.js
@@ -1,35 +1,3 @@
-// $(document).ready(function () {
-//   $("#view_button").click(getPicture);
-//   console.log("apod");
-// });
-// function getPicture() {
-//   var url =
-//     "https://api.nasa.gov/planetary/apod?api_key=gyhaJ2GQ5PF58E8w6iESVvscg2en3nrGcOqczI2s&date=" +
-//     $("#date").val();
-
-//   let fetcher = fetcher(url);
-
-//   fetcher
-//     .then((res) => res.json())
-//     .then((d) => {
-//       showPicture(d);
-//     })
-//     .catch((d) => {
-//       noPicture(d);
-//     });
-//   console.log("getpic");
-// }
-
-// function showPicture(data) {
-//   console.log("Picture: found");
-//   $("#pic").attr("src", data.url);
-//   $("#title").text(data.title);
-// }
-// function noPicture(error) {
-//   console.log("Picture: not found");
-//   alert(error.responseText);
-// }
-
 $(document).ready(function () {
   $("#view_button").click(getPicture);
   console.log("apod");
