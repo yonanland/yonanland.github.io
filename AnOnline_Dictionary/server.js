233945534b1bd6ded18795ecbf0e6ec116523da6
yonanland <yonininu@gmail.com>
2023-05-24 00:30:41 -0500

    updates online dictionary

--- AnOnline_Dictionary/server.js
+++ AnOnline_Dictionary/server.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 app.get("/word", function (req, res) {
-  // console.log('calling server...' + req.query.word);
   const word = req.query.word;
   console.log("calling server..." + word);
   conn.connect(function (err) {
-    // if (err) throw err;
     if (err) console.log(err.message);
     console.log("connected");
     const squery = conn.query(
       `SELECT * FROM entries where word = '${word}'  `,
       function (err, result) {
-        // if (err) throw err;
         console.log("end of query");
         if (err) console.log(err.message);
         res.json(result);
